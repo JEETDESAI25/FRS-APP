e51c5d58d72bbd3ec65b09e04cecbd74ef231063
JEETDESAI25 <jeetdesai25@gmail.com>
2023-04-05 21:44:06 -0700

    changes in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,12 +10,20 @@ module.exports = {
   overrides: [
   ],
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
   plugins: [
     'react',
+    'react-hooks',
   ],
   rules: {
+    'react/react-in-jsx-scope': 0,
+    'no-console': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
