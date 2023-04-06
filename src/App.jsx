b4c97b858087cf7c73e4164ee0db8e2ddb5ee604
JEETDESAI25 <jeetdesai25@gmail.com>
2023-04-05 23:06:23 -0700

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,142 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+/* eslint-disable react/prop-types */
+/* eslint-disable camelcase */
+
+import './App.css';
+import foodData from './Data';
+
+// const foodData = [
+//   {
+//     Name: 'Pizza',
+//     Calories: 500,
+//     Image_url: '',
+//     Ingredients: 'cheese',
+//     Price: 30,
+//     'Food Category': 'Italian',
+//     Id: 1,
+//   },
+//   {
+//     Name: 'Pasta',
+//     Calories: 590,
+//     Image_url: '',
+//     Ingredients: 'pasta',
+//     Price: 20,
+//     'Food Category': 'Italian',
+//     Id: 2,
+//   },
+//   {
+//     Name: 'Butter Chicken',
+//     Calories: 800,
+//     Image_url: '',
+//     Ingredients: 'butter',
+//     Price: 10,
+//     'Food Category': 'Indian',
+//     Id: 3,
+//   },
+//   {
+//     Name: 'Burrito',
+//     Calories: 700,
+//     Image_url: '',
+//     Ingredients: 'avocado',
+//     Price: 14,
+//     'Food Category': 'Mexican',
+//     Id: 4,
+//   },
+//   {
+//     Name: 'Chicken Bowl',
+//     Calories: 890,
+//     Image_url: '',
+//     Ingredients: 'beans',
+//     Price: 12,
+//     'Food Category': 'Mexican',
+//     Id: 5,
+//   },
+//   {
+//     Name: 'Afghani Chicken',
+//     Calories: 790,
+//     Image_url: '',
+//     Ingredients: 'chicken',
+//     Price: 14,
+//     'Food Category': 'Indian',
+//     Id: 6,
+//   },
+//   {
+//     Name: 'Sushi',
+//     Calories: 670,
+//     Image_url: '',
+//     Ingredients: 'fish',
+//     Price: 22,
+//     'Food Category': 'Japanese',
+//     Id: 7,
+//   },
+//   {
+//     Name: 'Hamburger',
+//     Calories: 980,
+//     Image_url: '',
+//     Ingredients: 'Pork',
+//     Price: 13,
+//     'Food Category': 'Burger',
+//     Id: 8,
+//   },
+//   {
+//     Name: 'Chicken Sandwich ',
+//     Calories: 800,
+//     Image_url: '',
+//     Ingredients: 'chicken',
+//     Price: 16,
+//     'Food Category': 'Burger',
+//     Id: 9,
+//   },
+// ];
+
+function FoodCard({
+  name, calories, imageUrl, ingredients, price, foodCategory,
+}) {
+  return (
+    <div className="food-card">
+      <img src={imageUrl} alt={name} className="food-image" />
+      <h2 className="food-title">{name}</h2>
+      <p className="food-description">
+        <strong>Calories:</strong>
+        {' '}
+        {calories}
+      </p>
+      <p className="food-description">
+        <strong>Ingredients:</strong>
+        {' '}
+        {ingredients}
+      </p>
+      <p className="food-description">
+        <strong>Price:</strong>
+        {' '}
+        $
+        {price}
+      </p>
+      <p className="food-description">
+        <strong>Food Category:</strong>
+        {' '}
+        {foodCategory}
+      </p>
+    </div>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <h1 className="header">Food Recommendations</h1>
+      <div className="food-list">
+        {foodData.map((food) => (
+          <FoodCard
+            key={food.Id}
+            name={food.Name}
+            calories={food.Calories}
+            imageUrl={food.Image_url}
+            ingredients={food.Ingredients}
+            price={food.Price}
+            foodCategory={food.FoodCategory}
+          />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
