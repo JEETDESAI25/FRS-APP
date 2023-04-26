feb87068d230242103621ba6d1aa7a76f1c35439
JEETDESAI25 <jeetdesai25@gmail.com>
2023-04-25 18:31:44 -0700

    Refactor App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/named */
 /* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable comma-dangle */
 /* eslint-disable operator-linebreak */
@@ -5,37 +6,10 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/prop-types */
 /* eslint-disable camelcase */
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import foodData from "./Data";
-
-function FoodCard({
-  name,
-  calories,
-  imageUrl,
-  ingredients,
-  price,
-  foodCategory,
-}) {
-  return (
-    <div className="food-card">
-      <img src={imageUrl} alt={name} className="food-image" />
-      <h2 className="food-title">{name}</h2>
-      <p className="food-description">
-        <strong>Calories:</strong> {calories}
-      </p>
-      <p className="food-description">
-        <strong>Ingredients:</strong> {ingredients}
-      </p>
-      <p className="food-description">
-        <strong>Price:</strong> ${price}
-      </p>
-      <p className="food-description">
-        <strong>Food Category:</strong> {foodCategory}
-      </p>
-    </div>
-  );
-}
+import FoodCard from "./components/FoodCard";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("All");
