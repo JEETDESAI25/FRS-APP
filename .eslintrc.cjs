020a99e7678e3b473464dae012268bc65458a8f8
JEETDESAI25 <jeetdesai25@gmail.com>
2023-04-25 18:31:31 -0700

    Changed eslint config file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,25 @@
+/* eslint-disable no-undef */
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  overrides: [
-  ],
+  overrides: [],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    'react-hooks',
-  ],
+  plugins: ["react", "react-hooks"],
   rules: {
-    'react/react-in-jsx-scope': 0,
-    'no-console': 0,
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
+    "react-refresh/only-export-components": "warn",
   },
 };
