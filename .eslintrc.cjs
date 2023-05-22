460eb72b251981ec6b713a85505fa39bdcaec0e2
JEETDESAI25 <jeetdesai25@gmail.com>
2023-05-22 12:53:02 -0700

    made required changes to eslint config file.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,20 @@
-/* eslint-disable no-undef */
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-  ],
-  overrides: [],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react", "react-hooks"],
-  rules: {
-    "react-refresh/only-export-components": "warn",
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', 'react-hooks', 'import'],
 };
