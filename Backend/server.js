030e0494f745a41cc4898474369aac6603133af6
JEETDESAI25 <jeetdesai25@gmail.com>
2023-06-09 18:40:07 -0700

    Added function to handle errors in ther server.

--- Backend/server.js
+++ Backend/server.js
@@ -10,3 +10,23 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
 	console.log(`Server listening at http://localhost:${port}`);
 });
+
+app.get('/', (req, res) => {
+	try {
+		// some code that might throw an error
+		res.send('Success!');
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Internal Server Error');
+	}
+});
+
+app.get('/', (req, res) => {
+    try {
+        res.send('success!');
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send
+    }
+})
