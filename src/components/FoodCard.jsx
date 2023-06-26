d8494c8e6c1c327fd444d2beebc7e3344bc6c689
JEETDESAI25 <jeetdesai25@gmail.com>
2023-06-26 03:20:29 -0700

    Updated FoodCard Component and Css.

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,3 +1,72 @@
+// import react from 'react';
+// import PropTypes from 'prop-types';
+// import '/Users/jd/Developer/FRS/FRS APP/src/styles/FoodCard.css';
+
+// function FoodCard({
+// 	name,
+// 	calories,
+// 	imageUrl,
+// 	price,
+// 	foodCategory,
+// 	ingredients,
+// }) {
+// 	const [rating, setRating] = react.useState('');
+// 	const [hoverRating, setHoverRating] = react.useState(0);
+
+// 	function handleRatingChange(value) {
+// 		setRating(value);
+// 		setHoverRating(value);
+// 	}
+
+// 	return (
+// 		<div className='food-card'>
+// 			<img className='food-image' src={imageUrl} alt={`${name}`} />
+// 			<h2 className='food-title'>{name}</h2>
+// 			<p className='food-description'>
+// 				Calories: {calories} <br />
+// 				Ingredients: {ingredients} <br />
+// 				Price: ${price} <br />
+// 				Category: {foodCategory}
+// 			</p>
+// 			<form
+// 				onSubmit={e => {
+// 					e.preventDefault();
+// 					console.log(`Rating for ${name}: ${rating}`);
+// 					setRating('');
+// 				}}
+// 			>
+// 				<label htmlFor={`rating-${name}`}>Rate this food:</label>
+// 				<div className='star-rating'>
+// 					{[1, 2, 3, 4, 5].map(starValue => (
+// 						<span
+// 							key={starValue}
+// 							className={`star${
+// 								(hoverRating || rating) >= starValue ? ' active' : ''
+// 							}`}
+// 							onClick={() => handleRatingChange(starValue)}
+// 							onMouseOver={() => setHoverRating(starValue)}
+// 						>
+// 							&#9733;
+// 						</span>
+// 					))}
+// 				</div>
+// 				<button type='submit'>Submit</button>
+// 			</form>
+// 		</div>
+// 	);
+// }
+
+// FoodCard.propTypes = {
+// 	id: PropTypes.string.isRequired,
+// 	name: PropTypes.string.isRequired,
+// 	calories: PropTypes.number.isRequired,
+// 	imageUrl: PropTypes.string.isRequired,
+// 	price: PropTypes.number.isRequired,
+// 	foodCategory: PropTypes.string.isRequired,
+// 	ingredients: PropTypes.string.isRequired,
+// };
+
+// export default FoodCard;
 import react from 'react';
 import PropTypes from 'prop-types';
 import '/Users/jd/Developer/FRS/FRS APP/src/styles/FoodCard.css';
@@ -10,14 +79,6 @@ function FoodCard({
 	foodCategory,
 	ingredients,
 }) {
-	const [rating, setRating] = react.useState('');
-	const [hoverRating, setHoverRating] = react.useState(0);
-
-	function handleRatingChange(value) {
-		setRating(value);
-		setHoverRating(value);
-	}
-
 	return (
 		<div className='food-card'>
 			<img className='food-image' src={imageUrl} alt={`${name}`} />
@@ -28,30 +89,7 @@ function FoodCard({
 				Price: ${price} <br />
 				Category: {foodCategory}
 			</p>
-			<form
-				onSubmit={e => {
-					e.preventDefault();
-					console.log(`Rating for ${name}: ${rating}`);
-					setRating('');
-				}}
-			>
-				<label htmlFor={`rating-${name}`}>Rate this food:</label>
-				<div className='star-rating'>
-					{[1, 2, 3, 4, 5].map(starValue => (
-						<span
-							key={starValue}
-							className={`star${
-								(hoverRating || rating) >= starValue ? ' active' : ''
-							}`}
-							onClick={() => handleRatingChange(starValue)}
-							onMouseOver={() => setHoverRating(starValue)}
-						>
-							&#9733;
-						</span>
-					))}
-				</div>
-				<button type='submit'>Submit</button>
-			</form>
+			<button>Select</button>
 		</div>
 	);
 }
