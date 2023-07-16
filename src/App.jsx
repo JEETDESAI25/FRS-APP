e119078354b609aee0334e7c1f5e59a42be0d983
JEETDESAI25 <jeetdesai25@gmail.com>
2023-07-16 04:40:33 -0700

    Updated.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import foodData from "./Data";
 import FoodCard from "./components/FoodCard";
